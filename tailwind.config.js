722c18bbaf82953cdea90db125f6fd82729ff73d
Gramazashvil1 <goga.ramazashvili@gmail.com>
2024-05-21 02:21:29 +0400

    Modify:Tailwind config settings

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'main-white': '#eeeeee'
+      },
+      fontFamily:{
+        'Roboto':['Roboto','sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
