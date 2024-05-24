34cfd2bb10269b6584c549475a1512daf765d7af
Gramazashvil1 <goga.ramazashvili@gmail.com>
2024-05-24 16:41:44 +0400

    Update: Modify styles, integrate Dayjs for date and time, display weather info from API

--- src/components/FirstSection/CurrentWeather/CurrentWeather.jsx
+++ src/components/FirstSection/CurrentWeather/CurrentWeather.jsx
@@ -1,22 +1,58 @@
-import Clear_Day from "/src/assets/images/clear-day.svg?react"
+import useWeatherData from "../../../hooks/useWeatherData.js";
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+import {useEffect, useState} from "react";
+import WeatherIcon from "../../WeatherIcon/WeatherIcon.jsx";
 
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 function CurrentWeather() {
+
+    // custom hook
+    const {weatherData} = useWeatherData();
+
+    // Current time
+    const receivedTimeZone = weatherData?.timezone;
+    const [currentTime, setCurrentTime] = useState(() => dayjs().tz(receivedTimeZone).format('HH : mm : ss'));
+
+    // Current date
+    const datetimeEpoch = weatherData?.currentConditions?.datetimeEpoch;
+    const date = dayjs.unix(datetimeEpoch).format('dddd D MMMM');
+
+    // Current time update
+    useEffect(() => {
+        const updateTime = () => {
+            setCurrentTime(dayjs().tz(receivedTimeZone).format('HH : mm : ss'));
+        };
+        setInterval(updateTime, 1000);
+    }, [receivedTimeZone]);
+
+    const {currentConditions: {temp, conditions, icon} = {}, address} = weatherData ?? {};
+
     return (
-        <section
-            className="w-full h-[280px] justify-center flex sm:w-[300px] p-[25px] border-[1px] border-[#00000009] bg-[#000] bg-opacity-20 min-w-[300px] rounded-2xl text-main-white lg:w-[300px] sm:justify-start">
+        <section className="w-full h-[280px] justify-center flex sm:w-[300px] p-[25px] border-[1px] border-[#00000009] bg-[#000] bg-opacity-20 min-w-[300px] rounded-2xl text-main-white lg:w-[300px] sm:justify-start">
             <div className="h-full flex flex-col justify-between">
                 <div>
-                    <h1 className="text-[25px] leading-[29px] line-clamp-1 ">Tbilisi</h1>
-                    <p className="text-[15px] leading-[18px]">Monday 06 February</p>
-                    <p className="text-[15px] leading-[18px]">21:07:01</p>
+                    <h1 className="capitalize text-[26px] leading-[29px] line-clamp-1">
+                        {address}
+                    </h1>
+                    <p className="text-[15px] leading-[19px] tracking-wide">
+                        {date}
+                    </p>
+                    <p className="text-[15px] leading-[18px] tracking-tighter">
+                        {currentTime}
+                    </p>
                 </div>
                 <div className="flex flex-row gap-x-[30px]  py-[15px]">
-                    <Clear_Day className="w-[90px] h-auto"/>
-                    <p className="flex items-center text-[60px] font-bold">24°</p>
+                    <WeatherIcon weather={icon} className="w-[95px] h-auto"/>
+                    <p className="flex items-center text-[55px] font-bold">
+                        {temp}°
+                    </p>
                 </div>
-                <p className="text-[15px] py-[1px] flex-wrap items-end flex line-clamp-2">
-                    Partially cloudy
+                <p className="text-[15px] py-[1px] flex-wrap items-end flex line-clamp-2 tracking-wide">
+                    {conditions}
                 </p>
             </div>
         </section>
