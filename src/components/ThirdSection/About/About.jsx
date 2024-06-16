92b9b0e6dbb86828aaaddfa3607496303cec562b
Gramazashvil1 <goga.ramazashvili@gmail.com>
2024-06-16 19:49:14 +0400

    Add: text content to About component

--- src/components/ThirdSection/About/About.jsx
+++ src/components/ThirdSection/About/About.jsx
@@ -2,13 +2,14 @@ import Card from "../../Card/Card.jsx";
 
 function About() {
     return (
-        <Card swiperDisable={true} title="About" className="w-full h-full sm:h-[280px] sm:justify-start lg:h-full pt-[60px] pb-[20px]">
+        <Card swiperDisable={true} title="About"
+              className="w-full h-full sm:h-[280px] sm:justify-start lg:h-full pt-[60px] pb-[20px]">
 
-             <p className="px-[25px] text-[15px]">
-                 Lorem sit amet, consectetur adipisicing elit. Blanditiis, neque, quidem  quisquam. Consequatur illum quia quo saepe volup
-                 Lorem dolor sit amet, consectetur adipisicing  Blanditiis, neque, quidem? Animi ex, quisquam. Consequatur illum quia quo saepe
-                 Lorem ipsum dolor sit amet, consectetur  elit. Blanditiis.
-             </p>
+            <p className="px-[25px] text-[15px]">
+                Welcome to our Weather Forecast Website! We provide accurate, up-to-date weather information, including
+                current conditions and detailed forecasts for the week and hour. Our user-friendly platform works
+                seamlessly on all devices, ensuring you can stay informed and plan with confidence, wherever you are.
+            </p>
 
         </Card>
     );
